5949b5d2af650014ac8520400449ad65fb7495be
BaranovIgor <igorbaranov512@gmail.com>
2026-09-24 16:11:32 +0300

    Update Dz2.c

--- Dz2.c
+++ Dz2.c
@@ -2,8 +2,9 @@
 #include<locale.h>
 int main() {
 	setlocale(LC_ALL,".UTF8");
-	int n = 100;
-	int x = 100;
-	printf("Сумма = %d фунтов", 5 * n * x / 4);
+	int n = 100;//количество пассажиров
+	int x = 100;//стоимость билета второго класса
+	int total =  5 * n * x / 4// вычисление общей суммы денег от продажи билетов:
+	printf("Сумма = %d фунтов", total);
 	return 0;
 }
